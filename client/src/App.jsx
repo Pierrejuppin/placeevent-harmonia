f183bc3654af794d564e7b6d6f7d76e270a4cde3
Pierre Juppin <pierre.juppin@gmail.com>
2024-07-17 16:40:22 +0200

    feat : creating connexionpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
 import "./index.css";
-/* import { useState, useEffect } from "react"; */
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-/* import fetchAuth from "./lib/auth"; */
+import fetchAuth from "./lib/auth";
 
 function App() {
-  /* const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
   useEffect(() => {
     fetchAuth().then((response) => setCurrentUser(response));
   }, []);
-  console.log(currentUser);
- */
+
   return (
     <>
       <ToastContainer
@@ -27,9 +26,8 @@ function App() {
         theme="colored"
         transition={Bounce}
       />
-      {/* <p>Votre nom est : {currentUser?.first_name}</p>
-      <Outlet context={{ currentUser, setCurrentUser }} /> */}
-      <Outlet />
+      <p>Votre nom est : {currentUser?.first_name}</p>
+      <Outlet context={{ currentUser, setCurrentUser }} />
     </>
   );
 }
